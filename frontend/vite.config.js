90d9073e39bd0dd3a5c04ae8c87312749747dd58
shashi <shashiranjankumar494@gmail.com>
2025-07-24 12:07:21 +0530

    resolving deployment issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
